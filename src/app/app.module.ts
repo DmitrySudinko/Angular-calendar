41608640ee09da55f1ea7672e1a1159950f7c1cb
Dmitry Sudinko <7774223@gmail.com>
2022-11-21 16:25:34 +0300

    Calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganaizerComponent } from './organaizer/organaizer.component';
+import {MomentPipe} from "./shared/moment.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganaizerComponent,
+    MomentPipe
   ],
   imports: [
     BrowserModule,
